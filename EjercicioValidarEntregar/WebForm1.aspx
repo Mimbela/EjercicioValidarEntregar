51575949006947b31109879c6761f27d5b6276dd
DesarrolloWeb <DesarrolloWeb@T06W03>
2018-05-31 17:11:56 +0200

    Agregar archivos de proyecto.

---
+++ EjercicioValidarEntregar/WebForm1.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EjercicioValidarEntregar.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Nombre de Usuario<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" 
+                runat="server" 
+                ControlToValidate="TextBox1"
+                Display="Dynamic"
+                Text="*"
+                ErrorMessage="Debes poner tu nombre">
+
+            </asp:RequiredFieldValidator>
+            <br />
+
+            Edad <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2"
+                runat="server" 
+                 ControlToValidate="TextBox2"
+                Text="*"
+                Display="Dynamic"
+               ErrorMessage="Debes introducir tu edad">
+
+            </asp:RequiredFieldValidator>
+            <asp:RangeValidator ID="RangeValidator1" 
+                runat="server" 
+                ControlToValidate="TextBox2"
+                Type="Integer"
+                MinimumValue="18"
+                MaximumValue="50"
+                ErrorMessage="Debes tener edad entre 18-50 años">
+
+            </asp:RangeValidator>
+
+            <br />
+
+            Password <asp:TextBox ID="TextBox3" TextMode="Password" runat ="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" 
+                runat="server" 
+                ControlToValidate="TextBox3"
+                Display="Dynamic"
+                Text="*"
+                
+                ErrorMessage="Introduce una contraseña.">
+            </asp:RequiredFieldValidator>
+
+            <asp:CustomValidator ID="CustomValidator1" 
+                runat="server" 
+                ControlToValidate="TextBox3"
+                
+               OnServerValidate="CustomValidator1_ServerValidate"
+               
+                ErrorMessage="Pon una contraseña de más de 4 caracteres">
+
+            </asp:CustomValidator>
+           <%-- si lo queremos validar desde el cliente:
+             debemos poner :clientvalidationfunction = "comprobarlongitud"
+               desde el servidorsería onservervalidate.
+               luego debemos poner el siguiente  script en el cliente:
+            <script type="text/javascript">
+                function Comprobarlongitud(fuente, args) {
+                    args.isValida= args.value.length>=4
+                }
+            </script>--%>
+             <br />
+
+            Confirmar Password <asp:TextBox ID="TextBox4" runat="server" TextMode="Password"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator4" 
+                runat="server" 
+                 ControlToValidate="TextBox4"
+                ErrorMessage="RequiredFieldValidator">
+
+            </asp:RequiredFieldValidator>
+            <asp:CompareValidator ID="CompareValidator1" 
+                ControlToValidate="TextBox4"
+                ControlToCompare="TextBox3"
+                runat="server" 
+                ErrorMessage="No coinciden las passwords">
+
+            </asp:CompareValidator>
+
+        <%--si hacemos la comparación desde el cliente debemos añadir lo siguiente    
+            controltovalidate=txtconfirPass
+            controltoconpare=txtpass--%>
+            
+            <br />
+
+            <asp:Button ID="Button1" runat="server" Text="ACEPTAR" />
+
+            <br />
+
+            <asp:ValidationSummary ID="ValidationSummary1" runat="server" />
+
+
+
+        </div>
+    </form>
+</body>
+</html>
+<%--Ejercicio propuesto(4.3):
+Crear un formulario web con los siguientes datos:
+Nombre Usuario(Requerido)
+Edad(Requerido  rango : 18-50)
+Password (requeridos mas de 4 caracteres)
+Confirmar Password (debe coincidir con la anterior)
+Botón de Envío.
+Realizar las validaciones mostrando un resumen de errores al clic del botón.--%>
---
+++ EjercicioValidarEntregar/WebForm1.aspx.designer.cs
@@ -0,0 +1,141 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EjercicioValidarEntregar {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// Control RequiredFieldValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
+        /// Control RangeValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
+        
+        /// <summary>
+        /// Control TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// Control RequiredFieldValidator3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        
+        /// <summary>
+        /// Control CustomValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator CustomValidator1;
+        
+        /// <summary>
+        /// Control TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        
+        /// <summary>
+        /// Control RequiredFieldValidator4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
+        
+        /// <summary>
+        /// Control CompareValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator1;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Control ValidationSummary1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+    }
+}
